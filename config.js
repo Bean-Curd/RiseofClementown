9a772525e4b2a0e5e776172f8fc85f17d5b37904
Ashley Goh <116317152+Bean-Curd@users.noreply.github.com>
2024-01-23 20:39:56 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC1SsfNhM7mRPciuFp4jDPk3PVZSCp_EeQ",
+  authDomain: "riseofclementown.firebaseapp.com",
+  databaseURL:
+    "https://riseofclementown-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "riseofclementown",
+  storageBucket: "riseofclementown.appspot.com",
+  messagingSenderId: "370532568050",
+  appId: "1:370532568050:web:0dabdda1409f0aa4a0396b",
+};
